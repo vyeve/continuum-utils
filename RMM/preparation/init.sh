03f18513226cf2dc633693082e85e89f18491bf7
vitaliyyevenko <vitaliy.yevenko@continuum.net>
2019-06-02 13:17:33 +0300

    update raw asset to json type

--- RMM/preparation/init.sh
+++ RMM/preparation/init.sh
@@ -3,13 +3,13 @@
 # docker rm -f pg
 # docker run  -d  --name pg -p 5432:5432 test
 # docker run -d -e PGDATA=/var/lib/postgresql/pgdata --name pg -p 5432:5432 postgres:11.3-alpine
-# docker run -d --name pg -p 5432:5432 vyeve/dg-asset:1.0
+docker run -d --name pg -p 5432:5432 -v ${PWD}/:/migrations vyeve/dg-asset:1.0
 # docker run -d --name pg -p 5432:5432 postgres:11.3-alpine
 
-# docker exec -it pg /bin/bash
+docker exec -it pg /bin/bash
 # psql -U postgres
 # psql -d dg -U postgres
 # copy asset to /migrations/data.txt
 
-docker login -u vyeve -p Cjvjys7495
+# docker login -u vyeve -p Cjvjys7495
 # docker login --username=vyeve --email=vyeve@softserveinc.com
--- RMM/preparation/sql/migrate.sql
+++ RMM/preparation/sql/migrate.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS asset (
   endpointID varchar(256) NOT NULL UNIQUE,
   partnerID varchar(32) NOT NULL,
-  rawAsset text,
+  rawAsset json,
   PRIMARY KEY (endpointID)
 );
 COPY asset FROM '/migrations/assets.csv' WITH CSV DELIMITER ',' HEADER
