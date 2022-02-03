ea45d01a1f4194389bc7ec117f92ac5424dfc2a2
vyeve <xobpaxa@ukr.net>
2022-02-03 20:24:36 +0200

    modified

--- RMM/Docker/Elasticsearch/docker-entrypoint.sh
+++ RMM/Docker/Elasticsearch/docker-entrypoint.sh
@@ -2,6 +2,5 @@
 
 # wait for Elasticsearch to start, then run the setup script to
 # create and configure the index.
-
 exec /usr/share/elasticsearch/utils/wait-for-it.sh localhost:9200 -- /usr/share/elasticsearch/config/setup.sh &
 exec $@ 
